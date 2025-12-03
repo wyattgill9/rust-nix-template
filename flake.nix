1ac3f6b0957dcfd5445c5862b030ecbb472ce488
wyattgill9 <wyattgill01@outlook.com>
2025-12-03 11:47:31 -0800

    modulizew

--- flake.nix
+++ flake.nix
@@ -17,70 +17,17 @@
     ...
   }: let
     systems = ["x86_64-linux" "aarch64-darwin"];
-
-    forAllSystems = f:
-      builtins.foldl' (
-        attrs: system: let
-          ret = f system;
-        in
-          builtins.foldl' (
-            attrs: key:
-              attrs
-              // {
-                ${key} =
-                  (attrs.${key} or {})
-                  // {
-                    ${system} = ret.${key};
-                  };
-              }
-          )
-          attrs (builtins.attrNames ret)
-      ) {}
-      systems;
-  in
-    forAllSystems (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        toolchain = fenix.packages.${system}.stable.toolchain;
-
-        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
-
-        commonArgs = {
-          src = craneLib.cleanCargoSource ./.;
-          strictDeps = true;
-
-          buildInputs = []
-            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-             pkgs.libiconv
-            ];
-        };
-
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-        rust-package = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-        });
-      in {
-        formatter = pkgs.writeShellScriptBin "formatter" ''
-          ${toolchain}/bin/cargo fmt
-        '';
-
-        checks = {
-          inherit rust-package;
-        };
-
-        packages.default = rust-package;
-
-        devShells.default = pkgs.mkShell {
-          packages = [
-            toolchain
-          ];
-
-          buildInputs = commonArgs.buildInputs;
-
-          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
-        };
-      }
-    );
+    
+    eachSystem = f: nixpkgs.lib.genAttrs systems (system: f {
+      inherit system;
+      pkgs = nixpkgs.legacyPackages.${system};
+      toolchain = fenix.packages.${system}.stable.toolchain;
+      craneLib = (crane.mkLib nixpkgs.legacyPackages.${system}).overrideToolchain 
+        fenix.packages.${system}.stable.toolchain;
+    });
+  in {
+    packages = eachSystem ({pkgs, craneLib, ...}: {
+      default = import ./package.nix {inherit pkgs craneLib;};
+    });
+  };
 }
---
+++ package.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  craneLib,
+}: let
+  src = craneLib.cleanCargoSource ./.;
+
+  commonArgs = {
+    inherit src;
+    strictDeps = true;
+
+    buildInputs =
+      []
+      ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+        pkgs.libiconv
+      ];
+  };
+
+  cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+in
+  craneLib.buildPackage (commonArgs
+    // {
+      inherit cargoArtifacts;
+    })
